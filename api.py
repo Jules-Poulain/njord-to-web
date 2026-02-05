ead4be643eb0535d29e01e431de82560eb385edd
Jules-Poulain <julot07.poulain@gmail.com>
2026-02-05 10:53:25 +0000

    API update

--- api.py
+++ api.py
@@ -8,3 +8,10 @@ app = FastAPI()
 async def boat_data():
     data = await get_latest_pgns()
     return data
+
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("Python_Example.api:app", host="0.0.0.0", port=port)
