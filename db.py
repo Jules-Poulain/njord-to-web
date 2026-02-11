598b9ff47182f37ecc41f4d49d25f81101f45b7f
Jules-Poulain <julot07.poulain@gmail.com>
2026-02-11 16:21:33 +0000

    new setup for dockhand

--- db.py
+++ db.py
@@ -1,9 +1,15 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database import Base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "njord")
+DB_USER = os.getenv("DB_USER", "njord")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "njordpass")
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
